d0fc2d549cdc90c51eb90103e9683576139024b3
elevyg <elevyg91@gmail.com>
2024-08-31 11:04:31 -0400

    chore: schedule data centralized

--- src/app/tasks.tsx
+++ src/app/tasks.tsx
@@ -14,6 +14,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
+import { schedule } from "~/data/program-data";
 
 const tasks = [
   { id: 1, name: "Compost", color: "bg-amber-600", emoji: "🍂" },
@@ -28,17 +29,6 @@ const tasks = [
 
 type Tasks = typeof tasks;
 type Task = Tasks[number];
-
-const schedule = [
-  { group: "Grupo 1", dateRange: "1 Oct - 30 Nov", tasks: [1, 2, 3, 4, 5, 6] },
-  { group: "Grupo 2", dateRange: "1 Oct - 30 Nov", tasks: [2, 3, 4, 5] },
-  { group: "Grupo 3", dateRange: "1 Oct - 30 Nov", tasks: [1, 7] },
-  { group: "Grupo 4", dateRange: "1 Oct - 30 Nov", tasks: [8] },
-  { group: "Grupo 5", dateRange: "1 Dic - 28 Feb", tasks: [1, 3, 4, 5, 6] },
-  { group: "Grupo 6", dateRange: "1 Dic - 28 Feb", tasks: [2, 3, 4, 5] },
-  { group: "Grupo 7", dateRange: "1 Dic - 28 Feb", tasks: [5, 7] },
-  { group: "Grupo 8", dateRange: "1 Dic - 28 Feb", tasks: [8] },
-];
 
 const TaskBox = ({ task, isActive }: { task: Task; isActive: boolean }) => (
   <TooltipProvider>
