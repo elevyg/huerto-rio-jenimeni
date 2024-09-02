18c5188b2f7995d38dc3c5a49a05055d60e48357
elevyg <elevyg91@gmail.com>
2024-09-02 17:02:28 -0400

    fix: url 2

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -1,5 +1,5 @@
 export const Constants = {
   baseUrl:
-    process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ??
+    `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}` ??
     "http://localhost:3000",
 };
