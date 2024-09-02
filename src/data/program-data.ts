69239acd29a6c1be8811f2d33a6063338beb5dbb
elevyg <elevyg91@gmail.com>
2024-09-02 13:14:40 -0400

    feat: correct program data

--- src/data/program-data.ts
+++ src/data/program-data.ts
@@ -1,10 +1,42 @@
 export const schedule = [
-  { group: "Grupo 1", dateRange: "1 Oct - 30 Nov", tasks: [1, 2, 3, 4, 5, 6] },
-  { group: "Grupo 2", dateRange: "1 Oct - 30 Nov", tasks: [2, 3, 4, 5] },
-  { group: "Grupo 3", dateRange: "1 Oct - 30 Nov", tasks: [1, 7] },
-  { group: "Grupo 4", dateRange: "1 Oct - 30 Nov", tasks: [8] },
-  { group: "Grupo 5", dateRange: "1 Dic - 28 Feb", tasks: [1, 3, 4, 5, 6] },
-  { group: "Grupo 6", dateRange: "1 Dic - 28 Feb", tasks: [2, 3, 4, 5] },
-  { group: "Grupo 7", dateRange: "1 Dic - 28 Feb", tasks: [5, 7] },
-  { group: "Grupo 8", dateRange: "1 Dic - 28 Feb", tasks: [8] },
+  {
+    group: "Grupo 1",
+    dateRange: "8 Sept - 22 Sept",
+    tasks: [1, 2, 3, 4, 5, 6, , 8],
+  },
+  {
+    group: "Grupo 2",
+    dateRange: "6 Oct - 20 Oct",
+    tasks: [1, 2, 3, 4, 5, 6, 7, 8],
+  },
+  {
+    group: "Grupo 3",
+    dateRange: "10 Nov - 24 Nov",
+    tasks: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    group: "Grupo 4",
+    dateRange: "8 Dic - 22 Dic",
+    tasks: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    group: "Grupo 5",
+    dateRange: "12 Ene - 26 Ene",
+    tasks: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    group: "Grupo 6",
+    dateRange: "9 Feb - 23 Feb",
+    tasks: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    group: "Grupo 7",
+    dateRange: "9 Mar - 23 Mar",
+    tasks: [1, 3, 4, 5, 6, 7, 8],
+  },
+  {
+    group: "Grupo 8",
+    dateRange: "6 Abr - 20 Abr",
+    tasks: [1, 5, 7, 8],
+  },
 ];
